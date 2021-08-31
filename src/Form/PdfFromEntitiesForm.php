f63ad0445b9058f88c3103c94f10702da346d136
ever <s.lavrynenko@internetdevels.com>
2021-08-31 21:23:30 +0300

    Added needed files and logic.

--- src/Form/PdfFromEntitiesForm.php
+++ src/Form/PdfFromEntitiesForm.php
@@ -4,6 +4,7 @@ namespace Drupal\pdf_from_entities\Form;
 
 use Drupal\Core\Batch\BatchBuilder;
 use Drupal\Core\Entity\EntityTypeBundleInfo;
+use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
@@ -16,13 +17,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *
  */
 class PdfFromEntitiesForm extends FormBase {
-
-  /**
-   * Entity Type Manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
 
   /**
    * An array with available node types.
@@ -46,18 +40,31 @@ class PdfFromEntitiesForm extends FormBase {
   protected $nodeStorage;
 
   /**
+   * The file system service.
+   *
+   * @var \Drupal\Core\File\FileSystemInterface
+   */
+  protected $fileSystem;
+
+  protected function getEnititiesFolder() :string {
+    return 'temporary://pdf_from_entities/';
+  }
+
+  /**
    * PdfFromEntitiesForm constructor.
    *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   EntityTypeManager construct.
+   * @param \Drupal\Core\File\FileSystemInterface $file_system
+   *    The file system service.
    */
-  public function __construct(EntityTypeBundleInfo $entity_type_bundle_info, NodeStorageInterface $node_storage, EntityTypeManagerInterface $entity_type_manager) {
+  public function __construct(EntityTypeBundleInfo $entity_type_bundle_info, NodeStorageInterface $node_storage, FileSystemInterface $file_system) {
     $this->nodeBundles = $entity_type_bundle_info->getBundleInfo('node');
     array_walk($this->nodeBundles, function (&$a) {
       $a = $a['label'];
     });
-    $this->entityTypeManager = $entity_type_manager;
     $this->nodeStorage = $node_storage;
+    $this->fileSystem = $file_system;
+    $path_pdf_folder = $this->getEnititiesFolder();
+    $this->fileSystem->prepareDirectory($path_pdf_folder, FileSystemInterface::CREATE_DIRECTORY);
     $this->batchBuilder = new BatchBuilder();
   }
 
@@ -68,7 +75,7 @@ class PdfFromEntitiesForm extends FormBase {
     return new static(
       $container->get('entity_type.bundle.info'),
       $container->get('entity_type.manager')->getStorage('node'),
-      $container->get('entity_type.manager')
+      $container->get('file_system')
       );
   }
 
@@ -98,14 +105,11 @@ class PdfFromEntitiesForm extends FormBase {
       '#button_type' => 'primary',
     ];
 
-    return $form;
-  }
+    $test = \Drupal::service('pdf_from_entities.generate_pdf');
 
-  /**
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    parent::validateForm($form, $form_state);
+    $test->getTestPDF();
+
+    return $form;
   }
 
   /**
@@ -113,13 +117,21 @@ class PdfFromEntitiesForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $entity_types = array_filter($form_state->getValue(['entity_types']));
+
+    foreach ($entity_types as $entity_type) {
+      $path_entity_folder =  'temporary://pdf_from_entities/' . $entity_type . '/';
+      $this->fileSystem->prepareDirectory($path_entity_folder, FileSystemInterface::CREATE_DIRECTORY);
+    }
+
+    $nodes = $this->getNodes($entity_types);
+
       $this->batchBuilder
         ->setTitle($this->t("Processing"))
         ->setInitMessage($this->t('Initializing.'))
         ->setProgressMessage($this->t('Completed @current of @total.'))
         ->setErrorMessage($this->t('An error has occurred.'));
 
-    $this->batchBuilder->addOperation([$this, 'processItems'], [$entity_types]);
+    $this->batchBuilder->addOperation([$this, 'processItems'], [$nodes]);
     $this->batchBuilder->setFinishCallback([$this, 'finished']);
     batch_set($this->batchBuilder->toArray());
   }
@@ -148,10 +160,7 @@ class PdfFromEntitiesForm extends FormBase {
 
       foreach ($context['sandbox']['items'] as $item) {
         if ($counter != $limit) {
-          $nodes = $this->getNodes($item);
-          // TODO: Prepare the folder for each content-type.
-          foreach ($nodes as $node) {
-          $this->processItem($node);
+          $this->processItem($item);
 
           $counter++;
           $context['sandbox']['progress']++;
@@ -164,7 +173,6 @@ class PdfFromEntitiesForm extends FormBase {
           // Increment total processed item values. Will be used in finished
           // callback.
           $context['results']['processed'] = $context['sandbox']['progress'];
-          }
         }
       }
     }
@@ -185,13 +193,21 @@ class PdfFromEntitiesForm extends FormBase {
     /** @var \Drupal\node\NodeInterface $node */
     $node = $this->nodeStorage->load($nid);
     $needed_fields = $node->getFields();
-    // TODO: Call the service that will be generating pdf-files from node
+
+    $test = \Drupal::service('pdf_from_entities.generate_pdf');
+
+    $test->getTestPDF();
+
+    // TODO: Call the service that will be generating pdf-files from node;
+    // TODO: Create logic in case If service return NULL;
   }
 
   /**
    * Finished callback for batch.
    */
   public function finished($success, $results, $operations) {
+    $this->fileSystem->deleteRecursive($this->getEnititiesFolder());
+
     $message = $this->t('Number of nodes affected by batch: @count', [
       '@count' => $results['processed'],
     ]);
@@ -202,7 +218,7 @@ class PdfFromEntitiesForm extends FormBase {
 
 
   /**
-   * Load all nids for specific type.
+   * Load all nids for specific types.
    *
    * @return array
    *   An array with nids.
@@ -210,7 +226,7 @@ class PdfFromEntitiesForm extends FormBase {
   public function getNodes($type) {
     return $this->nodeStorage->getQuery()
       ->condition('status', NodeInterface::PUBLISHED)
-      ->condition('type', $type)
+      ->condition('type', $type, 'IN')
       ->execute();
   }
 
